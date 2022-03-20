e36f3c8493649f6c0ae13f4eb82eaaaafb735ff1
coomanio0837 <coomanio0837@gmail.com>
2022-03-20 18:16:12 +0200

    middle code

---
+++ calc.js
@@ -0,0 +1,8 @@
+function inputDate(number){
+ var screen = document.getElementById('screen');
+ screen.value += number;
+}
+function clean(){
+    var screen = document.getElementById('screen');
+    screen.value = ""
+}
